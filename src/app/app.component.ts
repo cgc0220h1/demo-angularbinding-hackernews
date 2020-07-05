414371b26a005c8086bec61ada7708b8aca63408
Duc <vanduc2514@gmail.com>
2020-07-06 02:13:58 +0700

    Update two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,4 @@ export class AppComponent {
     title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
     url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
   };
-
-  updateArticle(title: any, url: any): void {
-    this.article.title = title;
-    this.article.url = url;
-  }
 }
